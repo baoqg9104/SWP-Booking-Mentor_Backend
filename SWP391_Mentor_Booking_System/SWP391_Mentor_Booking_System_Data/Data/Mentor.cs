4e74523042df792bd9199a165af0299dbc3565df
Vu Duc Anh <vuducanh37181@outlook.com.vn>
2024-09-26 11:00:10 +0700

    Add packages

---
+++ SWP391_Mentor_Booking_System/SWP391_Mentor_Booking_System_Data/Data/Mentor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391_Mentor_Booking_System_Data.Data
+{
+    public class Mentor
+    {
+        public int MentorId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public ICollection<MentorSkill> MentorSkills { get; set; }
+        public ICollection<MentorSlot> MentorSlots { get; set; }
+    }
+}
---
+++ SWP391_Mentor_Booking_System/SWP391_Mentor_Booking_System_Data/Data/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace SWP391_Mentor_Booking_System_Data.Data
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public ICollection<BookingRequest> BookingRequests { get; set; }
+        public ICollection<WalletTransaction> WalletTransactions { get; set; }
+    }
+
+}
