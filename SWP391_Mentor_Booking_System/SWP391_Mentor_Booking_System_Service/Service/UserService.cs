02b9c649156e1672426a1dd200ed90d0f26fe282
Vu Duc Anh <ducanhvu43@gmail.com>
2024-10-12 00:06:49 +0700

    add UpdateUser(all role)
    UserController, UserService, UpdateUserDTO

---
+++ SWP391_Mentor_Booking_System/SWP391_Mentor_Booking_System_Service/Service/UserService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWP391_Mentor_Booking_System_Data;
+using SWP391_Mentor_Booking_System_Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391_Mentor_Booking_System_Service.Service
+{
+    public class UserService
+    {
+        private readonly SWP391_Mentor_Booking_System_DBContext _context;
+
+        public UserService(SWP391_Mentor_Booking_System_DBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> UpdateUserAsync(UpdateUserDTO updateUserDto)
+        {
+            if(updateUserDto.role == "Student")
+            {
+                var user = await _context.Students.FirstOrDefaultAsync(u => u.StudentId == updateUserDto.Id);
+                if (user == null)
+                    return false;
+
+                user.Email = updateUserDto.Email;
+                user.StudentName = updateUserDto.Name;
+                user.Phone = updateUserDto.Phone;
+                user.Gender = updateUserDto.Gender;
+                user.DateOfBirth = updateUserDto.DateOfBirth;
+
+                _context.Students.Update(user);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+
+            if(updateUserDto.role == "Mentor")
+            {
+                var user = await _context.Mentors.FirstOrDefaultAsync(u => u.MentorId == updateUserDto.Id);
+                if (user == null)
+                    return false;
+
+                user.Email = updateUserDto.Email;
+                user.MentorName = updateUserDto.Name;
+                user.Phone = updateUserDto.Phone;
+                user.Gender = updateUserDto.Gender;
+                user.DateOfBirth = updateUserDto.DateOfBirth;
+
+                _context.Mentors.Update(user);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
